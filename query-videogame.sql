f8947b24d5f1a77a09285d27c93daf5ceec0f82b
luca-gif <luca.89m@gmail.com>
2022-09-28 16:29:16 +0200

    queries da 1 a 9

---
+++ query-videogame.sql
@@ -0,0 +1,48 @@
+﻿--9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
+SELECT *
+FROM videogames
+WHERE YEAR(release_date) = 2020;
+
+--8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
+SELECT *
+FROM reviews
+WHERE rating >= 2 AND rating <= 4;
+
+--7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
+SELECT *
+FROM category_videogame
+WHERE category_id = 2 OR category_id = 6;
+
+
+--6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
+SELECT *
+FROM awards
+WHERE description
+LIKE '%facere%';
+
+--5- Contare quanti tornei ci sono stati nell'anno 2015 (9)
+SELECT COUNT(*)
+FROM tournaments
+WHERE year = 2015
+GROUP BY year;
+
+--4- Selezionare tutte le recensioni scritte dal giocatore con ID = 800 (11)
+SELECT *
+FROM reviews
+WHERE player_id = 800;
+
+--3- Selezionare tutti i giocatori il cui nome finisce per "a" (220)
+SELECT *
+FROM players
+WHERE name
+LIKE '%a';
+
+--2- Selezionare tutti i giocatori della città di 'Rogahnland' (2)
+SELECT *
+FROM players
+WHERE city = 'Rogahnland';
+
+--1- Selezionare tutte le software house americane (3)
+SELECT *
+FROM software_houses
+WHERE country = 'united states';
